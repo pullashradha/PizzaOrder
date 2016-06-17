1d1d1a401050625ccc3cfdb7e92a60da5853c944
Shradha Pulla <pullashradha@gmail.com>
2016-06-17 09:12:21 -0700

    Show total price of custom pizza in final price column for 3 inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,52 @@
-//Business Logic
+// Business Logic
 function CustomPizza (size, sauce, topping1) {
   this.pizzaSize = size;
   this.sauce = sauce;
   this.topping1 = topping1;
   // this.topping2 = topping2;
   // this.topping3 = topping3;
+  this.price = 0;
 }
 
 CustomPizza.prototype.costCustom = function() {
   if (this.pizzaSize === "small") {
-    alert("Size = small");
+    this.price += 5;
   } else if (this.pizzaSize === "medium") {
-    alert("Size = medium");
+    this.price += 8;
   } else if (this.pizzaSize === "large") {
-    alert("Size = large");
+    this.price += 11;
   }
 
   if (this.sauce === "tomato") {
-    alert("Sauce = tomato");
+    this.price += 1;
   } else if (this.sauce === "alfredo") {
-    alert("Sauce = alfredo");
-  } else if (this.sauce === "no-sauce") {
-    alert("No sauce");
+    this.price += 1;
   }
 
   if (this.topping1 === "cheese") {
-    alert("Cheese");
+    this.price += 1;
   } else if (this.topping1 === "light-cheese") {
-    alert("Light cheese");
-  } else if (this.topping1 === "no-cheese") {
-    alert("No cheese");
+    this.price += 0.5;
   }
 }
+
 
 
 //User Interface Logic
 $(document).ready(function(event) {
 
-  var newCustomPizza = new CustomPizza(size, sauce, topping1);
-
   $("#pickup-btn").click(function(event) {
     $("#order-content").show();
     $("#landing-content").hide();
   });
-
   $("#delivery-btn").click(function(event) {
     $("#address").show();
     $("#pickup-btn,#delivery-btn,#landing-tagline").hide();
   });
-
   $("#order-proceed-btn").click(function(event) {
     $("#order-content").show();
     $("#landing-content").hide();
   });
-
   $("#custom-pizza-btn").click(function(event) {
     $("#custom-pizza").slideToggle();
   });
@@ -62,11 +55,13 @@ $(document).ready(function(event) {
     var size = $("select#size").val();
     var sauce = $("select#sauce").val();
     var topping1 = $("select#topping1").val();
+    var topping2 = $("select#topping2").val();
+    var topping3 = $("select#topping3").val();
 
+    var newCustomPizza = new CustomPizza(size, sauce, topping1);
     newCustomPizza.costCustom();
 
-    // $("#final-cost").text(newCustomPizza.pizzaSize);
-
+    $("#final-cost").text(newCustomPizza.price);
   });
 
 });
